997f88a0be5dbaad904f84c0b0fc336a0855918b
birdex <sameerpesari@gmail.com>
2025-08-05 13:43:44 -0700

    mobile scaling fix v2

--- indexTheme.js
+++ indexTheme.js
@@ -2,40 +2,55 @@ var themeCur = false;
 var themeWatcher = document.getElementById('lighting_mode');
 
 themeWatcher.addEventListener('click', () => {
-    if(!themeCur){
+    if (!themeCur) {
         document.getElementById("pagestyle").setAttribute("href", "indexDarkStyle.css");
-        themeWatcher.textContent="☾";
+        themeWatcher.textContent = "☾";
         themeCur = true;
-    }else{
+    } else {
         document.getElementById("pagestyle").setAttribute("href", "indexLightStyle.css");
-        themeWatcher.textContent="☼"
+        themeWatcher.textContent = "☼";
         themeCur = false;
     }
+
+    setTimeout(applyMobileStyles, 100); // 100ms delay
 });
+
+
+function applyMobileStyles() {
+    if (/Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        var buttons = document.getElementsByClassName('buttons');
+        var top = document.getElementById('top');
+        var header = document.querySelector('header');
+        var intro = document.querySelector('.intro');
+        var themeWatcher = document.getElementById('lighting_mode');
+        var home = document.getElementById('home');
+
+        top.style.marginTop = "4em";
+        themeWatcher.style.height = "65px";
+
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.flexDirection = "column";
+            buttons[i].style.gap = "5em";
+        }
+
+        header.style.width = "78%";
+        header.style.height = "80px"
+        home.style.width = "45px";
+        home.style.height = "65px";
+        header.style.transformOrigin = "top";
+        header.style.transform = "scale(1.3)";
+        intro.style.transformOrigin = "top";
+        intro.style.transform = "scale(1.3)";
+    }
+}
+
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
-        if (/Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-            var buttons = document.getElementsByClassName('buttons');
-            var top = document.getElementById('top');
-            var header = document.querySelector('header');
-            var intro = document.querySelector(".intro");
-
-            top.style.marginTop = "4em";
-            themeWatcher.style.height = "40px"
-
-            for (var i = 0; i < buttons.length; i++) {
-                buttons[i].style.flexDirection = "column";
-                buttons[i].style.gap = "5em";
-            }
-
-            header.style.width="78%"
-            header.style.transformOrigin="top";
-            header.style.transform="scale(1.3)";
-            intro.style.transformOrigin = "top";
-            intro.style.transform = "scale(1.3)";
-
-        }
+    applyMobileStyles();
 });
+
 
 
 
