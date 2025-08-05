76cdd6d456c016e245e747c538a1b005ea9c4d3c
birdex <sameerpesari@gmail.com>
2025-08-05 13:08:02 -0700

    scaling for mobile users

--- indexTheme.js
+++ indexTheme.js
@@ -15,15 +15,26 @@ themeWatcher.addEventListener('click', () => {
 
 document.addEventListener('DOMContentLoaded', function () {
         if (/Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        var buttons = document.getElementsByClassName('buttons');
-        var top = document.getElementById('top')
-        top.style.marginTop = "8em";
-        themeWatcher.style.height = "80px"
-        for (var i = 0; i < buttons.length; i++) {
-            buttons[i].style.flexDirection = "column";
-            buttons[i].style.gap = "5em";
+            var buttons = document.getElementsByClassName('buttons');
+            var top = document.getElementById('top');
+            var header = document.querySelector('header');
+            var intro = document.querySelector(".intro");
+
+            top.style.marginTop = "4em";
+            themeWatcher.style.height = "40px"
+
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].style.flexDirection = "column";
+                buttons[i].style.gap = "5em";
+            }
+
+            header.style.width="78%"
+            header.style.transformOrigin="top";
+            header.style.transform="scale(1.3)";
+            intro.style.transformOrigin = "top";
+            intro.style.transform = "scale(1.3)";
+
         }
-    }
 });
 
 
