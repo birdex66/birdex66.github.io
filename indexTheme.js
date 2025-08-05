2faadb731803b2ba97a0180c18d6154f2e820d8e
birdex <sameerpesari@gmail.com>
2025-08-04 23:25:13 -0700

    Init commit

---
+++ indexTheme.js
@@ -0,0 +1,30 @@
+var themeCur = false;
+var themeWatcher = document.getElementById('lighting_mode');
+
+themeWatcher.addEventListener('click', () => {
+    if(!themeCur){
+        document.getElementById("pagestyle").setAttribute("href", "indexDarkStyle.css");
+        themeWatcher.textContent="☾";
+        themeCur = true;
+    }else{
+        document.getElementById("pagestyle").setAttribute("href", "indexLightStyle.css");
+        themeWatcher.textContent="☼"
+        themeCur = false;
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+        if (/Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        var buttons = document.getElementsByClassName('buttons');
+        var top = document.getElementById('top')
+        top.style.marginTop = "8em";
+        themeWatcher.style.height = "80px"
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.flexDirection = "column";
+            buttons[i].style.gap = "5em";
+        }
+    }
+});
+
+
+
---
+++ readme.txt
@@ -0,0 +1,9 @@
+# hosted on GitHub Pages
+# designed thru Figma
+Gradients:
+    Light Mode: https://uigradients.com/#Petrol
+    Dark Mode: https://uigradients.com/#Lawrencium
+    
+
+# Static Website
+
